e54fe2b27498ff077ea339c1202432dad99a5c5e
Andrius <andrius.andrijauskas@icloud.com>
2020-07-15 20:20:24 -0400

    adding mongo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 require('./services/passport');
+require('./models/User');
 
+mongoose.connect(keys.mongoURI)
 const app = express();
 
 //authRoutes(app); --- equivalent below, require return a function and we pass to it
