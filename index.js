d90c812be94e46314acf70fe84ecd70d1106ba9c
Andrius <andrius.andrijauskas@icloud.com>
2020-07-19 09:03:21 -0400

    adding cookie and logout

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
-require('./services/passport');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI)
+
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //authRoutes(app); --- equivalent below, require return a function and we pass to it
 require('./routes/authRoutes')(app);
